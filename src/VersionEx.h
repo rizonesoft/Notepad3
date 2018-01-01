c9a878f79056f5beb3bf409a3fb6a20625436b53
Derick Payne <rizonetech@gmail.com>
2018-01-02 01:49:07 +0200

    Attempting to remedy build failure.

--- src/VersionEx.h
+++ src/VersionEx.h
@@ -4,8 +4,8 @@
 //                                                         //
 // //////////////////////////////////////////////////////////
 #define VERSION_MAJOR 3
-#define VERSION_MINOR 17
-#define VERSION_REV 1228
-#define VERSION_BUILD 783
+#define VERSION_MINOR 18
+#define VERSION_REV 102
+#define VERSION_BUILD 784
 #define SCINTILLA_VER 402
 #define ONIGMO_REGEX_VER 6.1.3
