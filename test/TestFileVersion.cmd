8582c6abfb1df7b7b9b1c27482e68c5db07798f7
Derick Payne <rizonetech@gmail.com>
2018-01-02 02:49:34 +0200

    Still fixing version test

--- test/TestFileVersion.cmd
+++ test/TestFileVersion.cmd
@@ -57,7 +57,7 @@ goto:EOF
 for /f "tokens=2 delims==" %%a in ('
     wmic OS Get localdatetime /value
 ') do set "dt=%%a"
-set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,1%" & set "DD=%dt:~6,2%"
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%Month%" & set "DD=%dt:~6,2%"
 set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
 ::set "datestamp=%YYYY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
 ::set "fullstamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
