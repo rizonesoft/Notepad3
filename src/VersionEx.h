e47abebaf005c5dcd2f9b9d09e7a8d3ecf9f00c6
Derick Payne <rizonetech@gmail.com>
2018-01-02 02:08:54 +0200

    Fixing Version.ps1 script.

--- src/VersionEx.h
+++ src/VersionEx.h
@@ -5,7 +5,7 @@
 // //////////////////////////////////////////////////////////
 #define VERSION_MAJOR 3
 #define VERSION_MINOR 18
-#define VERSION_REV 102
-#define VERSION_BUILD 784
+#define VERSION_REV 0102
+#define VERSION_BUILD 794
 #define SCINTILLA_VER 402
 #define ONIGMO_REGEX_VER 6.1.3
