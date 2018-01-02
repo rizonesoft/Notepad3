9edc4b95b40dda9640307aabe93cc38d1fdfd1db
Derick Payne <rizonetech@gmail.com>
2018-01-02 02:44:53 +0200

    Last attempt to fix version testing

--- test/TestFileVersion.cmd
+++ test/TestFileVersion.cmd
@@ -9,7 +9,7 @@ set NP3_BUILD_VER=%SCRIPT_DIR%..\Versions\build.txt
 :: --------------------------------------------------------------------------------------------------------------------
 
 set YY=00
-set MM=0
+set MM=00
 set DD=00
 call :GETDATE
 set BUILD=0
@@ -57,7 +57,7 @@ goto:EOF
 for /f "tokens=2 delims==" %%a in ('
     wmic OS Get localdatetime /value
 ') do set "dt=%%a"
-set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,1%" & set "DD=%dt:~6,2%"
 set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
 ::set "datestamp=%YYYY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
 ::set "fullstamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
