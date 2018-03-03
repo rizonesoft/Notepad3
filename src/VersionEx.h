c81afcfd26939b44557e9987596eb7e493731790
Derick Payne <rizonetech@gmail.com>
2018-03-03 16:13:24 +0200

    Releasing RC Build 916

--- src/VersionEx.h
+++ src/VersionEx.h
@@ -5,9 +5,9 @@
 // //////////////////////////////////////////////////////////
 #define VERSION_MAJOR 3
 #define VERSION_MINOR 18
-#define VERSION_REV 227
-#define VERSION_BUILD 911
+#define VERSION_REV 303
+#define VERSION_BUILD 916
 #define SCINTILLA_VER 403
 #define ONIGMO_REGEX_VER 6.1.3
-#define VERSION_BETA L" beta "
-#define VERSIONA_BETA " beta "
+#define VERSION_BETA L" "
+#define VERSIONA_BETA " "
