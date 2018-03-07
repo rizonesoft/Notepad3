b4f1e563aa9a2536d462d090e347b7ed0a752e56
Rainer Kottenhoff <rainer.kottenhoff@gmail.com>
2018-03-07 09:13:47 +0100

    + add: version string patching commands for different local builds

---
+++ Version_RC.cmd
@@ -0,0 +1,2 @@
+@echo off
+Version -VerPatch "RC"
--- src/VersionEx.h
+++ src/VersionEx.h
@@ -5,9 +5,9 @@
 // //////////////////////////////////////////////////////////
 #define VERSION_MAJOR 3
 #define VERSION_MINOR 18
-#define VERSION_REV 303
-#define VERSION_BUILD 916
+#define VERSION_REV 307
+#define VERSION_BUILD 918
 #define SCINTILLA_VER 403
 #define ONIGMO_REGEX_VER 6.1.3
-#define VERSION_BETA L" "
-#define VERSIONA_BETA " "
+#define VERSION_PATCH L""
+#define VERSIONA_PATCH ""
