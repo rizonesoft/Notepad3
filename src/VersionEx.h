a5b86a91dae865a58b5df7f8153a5842c8eaedea
Rainer Kottenhoff <rainer.kottenhoff@gmail.com>
2018-04-18 16:28:45 +0200

    + fix: missing files for Scintilla Lib v404

--- src/VersionEx.h
+++ src/VersionEx.h
@@ -5,9 +5,9 @@
 // //////////////////////////////////////////////////////////
 #define VERSION_MAJOR 3
 #define VERSION_MINOR 18
-#define VERSION_REV 329
-#define VERSION_BUILD 960
-#define SCINTILLA_VER 403
+#define VERSION_REV 418
+#define VERSION_BUILD 978
+#define SCINTILLA_VER 404
 #define ONIGMO_REGEX_VER 6.1.3
 #define VERSION_PATCH L" develop"
 #define VERSIONA_PATCH " develop"
