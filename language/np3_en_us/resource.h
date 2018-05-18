076d38a0d8812a20c71750c1b3b7784c13fb3981
Rainer Kottenhoff <rainer.kottenhoff@gmail.com>
2018-05-18 09:01:06 +0200

    + add: en-US for consistency restructuring

---
+++ language/np3_en_us/resource.h
@@ -0,0 +1,15 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by np3_en_us.rc
+//
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        50001
+#define _APS_NEXT_COMMAND_VALUE         40606
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
