fd3ebb04ae419d09acc05e347e3fc7fda498fe68
Rainer Kottenhoff <rainer.kottenhoff@gmail.com>
2018-07-17 21:33:35 +0200

    + add:  Notepad3 language resources for zn-CH

---
+++ language/np3_zh_cn/resource.h
@@ -0,0 +1,16 @@
+//{{NO_DEPENDENCIES}}
+// Von Microsoft Visual C++ generierte Includedatei.
+// Verwendet durch strings_zh_cn.rc
+//
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        0
+#define _APS_NEXT_COMMAND_VALUE         0
+#define _APS_NEXT_CONTROL_VALUE         0
+#define _APS_NEXT_SYMED_VALUE           0
+#endif
+#endif
