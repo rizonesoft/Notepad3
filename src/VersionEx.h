aa65cbd2f5fc379d3a3583cbd4e89fc85171b317
Rainer Kottenhoff <rainer.kottenhoff@gmail.com>
2018-11-03 00:09:07 +0100

    + add: menu to clear saved default position

--- src/VersionEx.h
+++ src/VersionEx.h
@@ -6,8 +6,8 @@
 #define APPNAME "Notepad3"
 #define VERSION_MAJOR 5
 #define VERSION_MINOR 18
-#define VERSION_REV 1030
-#define VERSION_BUILD 1334
+#define VERSION_REV 1103
+#define VERSION_BUILD 1339
 #define SCINTILLA_VER 412
 #define ONIGMO_REGEX_VER 6.1.3
 #define VERSION_PATCH " RC_NewMenu"
