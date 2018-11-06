8f3e2429903c6387bde467f4ec6bd35e91eaaef4
Derick Payne <rizonetech@gmail.com>
2018-11-06 22:54:15 +0200

    Releasing RC2 Build 1434

--- src/VersionEx.h
+++ src/VersionEx.h
@@ -6,8 +6,8 @@
 #define APPNAME "Notepad3"
 #define VERSION_MAJOR 5
 #define VERSION_MINOR 18
-#define VERSION_REV 1103
-#define VERSION_BUILD 1339
+#define VERSION_REV 1106
+#define VERSION_BUILD 1434
 #define SCINTILLA_VER 412
 #define ONIGMO_REGEX_VER 6.1.3
-#define VERSION_PATCH " RC_NewMenu"
+#define VERSION_PATCH " RC"
