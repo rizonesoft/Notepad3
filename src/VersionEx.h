8e032cbc17109e87040a63590cea8778593790a7
Derick Payne <rizonetech@gmail.com>
2019-01-08 18:18:55 +0200

    Preparing Final Release (Build 1602)

--- src/VersionEx.h
+++ src/VersionEx.h
@@ -6,8 +6,8 @@
 #define APPNAME "Notepad3"
 #define VERSION_MAJOR 5
 #define VERSION_MINOR 19
-#define VERSION_REV 107
-#define VERSION_BUILD 1595
+#define VERSION_REV 108
+#define VERSION_BUILD 1602
 #define SCINTILLA_VER 412
 #define ONIGMO_REGEX_VER 6.1.3
 #define VERSION_PATCH ""
