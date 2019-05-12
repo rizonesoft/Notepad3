0460c7bc287ceb1f7194bc559e84eb7f47f46b5e
Rainer Kottenhoff <rainer.kottenhoff@gmail.com>
2019-05-13 01:46:39 +0200

    + fix: ignoring "encrypted raw" loading for inconsisteny checks (typedefs missing)

--- src/TypeDefs.h
+++ src/TypeDefs.h
@@ -543,6 +543,7 @@ typedef struct _editfileiostatus
 
   bool bCancelDataLoss;
   bool bUnknownExt;
+  bool bEncryptedRaw;
 
   // inconsistent indentation
   INDENT_TYPE iGlobalIndent;
@@ -550,7 +551,7 @@ typedef struct _editfileiostatus
 
 } EditFileIOStatus;
 
-#define INIT_FILEIO_STATUS { CPI_ANSI_DEFAULT, SC_EOL_CRLF, false, false, false, {0,0,0}, false, false, I_MIX_LN, {0,0,0,0,0} }
+#define INIT_FILEIO_STATUS { CPI_ANSI_DEFAULT, SC_EOL_CRLF, false, false, false, {0,0,0}, false, false, false, I_MIX_LN, {0,0,0,0,0} }
 
 
 // ---------   common defines   --------
