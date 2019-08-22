a69ee17a25cd1d26df7424a4b85b67afb78dc52c
Pairi Daiza <hpwamr@outlook.com>
2019-08-22 23:31:59 +0200

    + upd: MSVC toolset version number

--- src/Version.h
+++ src/Version.h
@@ -72,7 +72,7 @@
 #if defined(_MSC_VER)
     #if (_MSC_VER >= 1922)
         #if(_MSC_FULL_VER >= 192227905) 
-            #define VER_CPL     MS Visual C++ 2019 v16.2.(0-2)
+            #define VER_CPL     MS Visual C++ 2019 v16.2.(0-3)
         #endif
     #elif (_MSC_VER >= 1921)
         #if(_MSC_FULL_VER >= 192127702) 
