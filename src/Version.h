5322cecc312722b3f65201f60bf1687a40044360
Pairi Daiza <hpwamr@outlook.com>
2019-11-21 21:44:10 +0100

    + Ver: VS2019 version update - VS 16.3.10 and update Changes.txt

--- src/Version.h
+++ src/Version.h
@@ -75,7 +75,7 @@
 #if defined(_MSC_VER)
     #if (_MSC_VER == 1923)
         #if(_MSC_FULL_VER >= 192328107) 
-            #define VER_CPL     MS Visual C++ 2019 v16.3.9
+            #define VER_CPL     MS Visual C++ 2019 v16.3.(9-10)
         #elif(_MSC_FULL_VER >= 192328106) 
             #define VER_CPL     MS Visual C++ 2019 v16.3.(3-8)
         #elif(_MSC_FULL_VER >= 192328105) 
