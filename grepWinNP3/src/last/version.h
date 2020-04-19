caa414368d43ca36bfe01fb07fbe328d6de2a148
Rainer Kottenhoff <rainer.kottenhoff@gmail.com>
2020-04-19 12:22:23 +0200

    + fix:  DLL-Build solution, editorconfigs, grepWin-Layout

--- grepWinNP3/src/last/version.h
+++ grepWinNP3/src/last/version.h
@@ -7,12 +7,12 @@
 //#pragma message(__LOC__"Run the NAnt script to get proper version info")
 
 #define FILEVER         2, 1, 0, 5
-#define PRODUCTVER      2, 1, 0, 5
-#define STRFILEVER      "2.1.0.5\0"
-#define STRPRODUCTVER   "2.1.0.5\0"
+#define PRODUCTVER      2, 1, 0, 6
+#define STRFILEVER      "2.1.0.6\0"
+#define STRPRODUCTVER   "2.1.0.6\0"
 
 #define GREPWIN_VERMAJOR     2
 #define GREPWIN_VERMINOR     1
 #define GREPWIN_VERMICRO     0
-#define GREPWIN_VERBUILD     5
-#define GREPWIN_VERDATE      "2020-04-18"
+#define GREPWIN_VERBUILD     6
+#define GREPWIN_VERDATE      "2020-04-19"
