382dc513a90b5e961fcd60ecae3f402415e563d6
Pairi Daiza <hpwamr@outlook.com>
2020-08-29 18:47:55 +0200

    Preparation Version files for RC_5.20.829.2

--- Version.cmd
+++ Version.cmd
@@ -12,9 +12,9 @@ PowerShell.exe -NoProfile -NonInteractive -NoLogo -ExecutionPolicy Unrestricted 
 set EXITCODE=%ERRORLEVEL%
 ::ECHO ERRORLEVEL=%EXITCODE%
 
-:: Pause for 5 seconds to verify the "Notepad3 version number:" before exiting
+:: Pause for 4 seconds to verify the "Notepad3 version number:" before exiting
 :: ============================================================================
-ping -n 6 127.0.0.1>nul
+ping -n 5 127.0.0.1>nul
 
 goto :END
 
