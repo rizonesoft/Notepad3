0870134c1cd5d7712cc4c677680991214badd821
RaiKoHoff <rainer.kottenhoff@gmail.com>
2020-09-21 09:10:33 +0200

    + upd: grepWinNP3 version bump

--- grepWinNP3/src/last/version.h
+++ grepWinNP3/src/last/version.h
@@ -6,13 +6,13 @@
 
 //#pragma message(__LOC__"Run the NAnt script to get proper version info")
 
-#define FILEVER         2, 1, 3, 30
-#define PRODUCTVER      2, 1, 3, 30
-#define STRFILEVER      "2.1.3.30\0"
-#define STRPRODUCTVER   "2.1.3.30\0"
+#define FILEVER         2, 1, 4, 30
+#define PRODUCTVER      2, 1, 4, 30
+#define STRFILEVER      "2.1.4.30\0"
+#define STRPRODUCTVER   "2.1.4.30\0"
 
 #define GREPWIN_VERMAJOR     2
 #define GREPWIN_VERMINOR     1
-#define GREPWIN_VERMICRO     3
+#define GREPWIN_VERMICRO     4
 #define GREPWIN_VERBUILD     30
-#define GREPWIN_VERDATE      "2020-09-20"
+#define GREPWIN_VERDATE      "2020-09-21"
