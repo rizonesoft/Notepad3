6a175d2c4bbf778d72025d4af4478de3d604e6a4
0x084E <0x084E@mailbox.org>
2020-09-30 12:25:22 +0200

    Add apply to SQL keywords

--- src/StyleLexers/styleLexSQL.c
+++ src/StyleLexers/styleLexSQL.c
@@ -5,7 +5,7 @@
 KEYWORDLIST KeyWords_SQL = {
 // Keywords
 "abort accessible account action active add admin after against aggregate algorithm all allocate alter "
-"always analyse analyze and any as asc ascii asensitive at attach audit authorization authors "
+"always analyse analyze and any apply as asc ascii asensitive at attach audit authorization authors "
 "auto_increment autoextend_size autoincrement avg avg_row_length backup before begin between binlog block "
 "both break btree buckets by cache call cascade cascaded case cast catalog_name chain change changed "
 "channel charset check checksum cipher class_origin client clone close cluster coalesce code collate "
