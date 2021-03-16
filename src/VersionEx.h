0a9a0c8c0492c4ded273790633771e5edc235e22
Pairi Daiza <hpwamr@outlook.com>
2021-03-16 14:40:00 +0100

    + Upd: Version files and Changes.txt

--- src/VersionEx.h
+++ src/VersionEx.h
@@ -8,8 +8,8 @@
 #define SAPPNAME "Notepad3"
 #define VERSION_MAJOR 5
 #define VERSION_MINOR 21
-#define VERSION_REV 312
-#define VERSION_BUILD 2
+#define VERSION_REV 316
+#define VERSION_BUILD 1
 #define SCINTILLA_VER 500
 #define LEXILLA_VER 500
 #define ONIGURUMA_REGEX_VER 6.9.6
