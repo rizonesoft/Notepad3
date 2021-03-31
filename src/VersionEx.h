360e1b13569aa1d4ab05406613acf3dcd9070f30
Pairi Daiza <hpwamr@outlook.com>
2021-03-31 18:11:31 +0200

    + Upd: Version files, Changes.txt and MS VS 2019 version update (v16.9.3)

--- src/VersionEx.h
+++ src/VersionEx.h
@@ -8,7 +8,7 @@
 #define SAPPNAME "Notepad3"
 #define VERSION_MAJOR 5
 #define VERSION_MINOR 21
-#define VERSION_REV 329
+#define VERSION_REV 331
 #define VERSION_BUILD 1
 #define SCINTILLA_VER 500
 #define LEXILLA_VER 500
