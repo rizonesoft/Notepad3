e6479e9ac10259f0dc9bfb9ffb3916c54669abb7
Rainer Kottenhoff <rainer.kottenhoff@gmail.com>
2021-04-26 19:11:12 +0200

    + prp: prepare Application Manifest for to grant identity for non-package desktop apps

--- src/VersionEx.h
+++ src/VersionEx.h
@@ -8,7 +8,7 @@
 #define SAPPNAME "Notepad3"
 #define VERSION_MAJOR 5
 #define VERSION_MINOR 21
-#define VERSION_REV 425
+#define VERSION_REV 426
 #define VERSION_BUILD 1
 #define SCINTILLA_VER 501
 #define LEXILLA_VER 501
@@ -17,4 +17,4 @@
 #define TINYEXPR_VER 2018.05.11
 #define UTHASH_VER 2.1.0
 #define VERSION_PATCH beta
-#define VERSION_COMMIT_ID dkt1-amr
+#define VERSION_COMMIT_ID nebukadn
