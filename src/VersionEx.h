5c8b8c89519057549035b4693edd4b9dfa00210f
Pairi Daiza <hpwamr@outlook.com>
2021-07-02 20:42:03 +0200

    + Upd: Version files and Changes.txt

--- src/VersionEx.h
+++ src/VersionEx.h
@@ -9,7 +9,7 @@
 #define VERSION_MAJOR 5
 #define VERSION_MINOR 21
 #define VERSION_REV 702
-#define VERSION_BUILD 4
+#define VERSION_BUILD 5
 #define SCINTILLA_VER 510
 #define LEXILLA_VER 510
 #define ONIGURUMA_REGEX_VER 7.0.0
@@ -17,4 +17,4 @@
 #define TINYEXPR_VER 2018.05.11
 #define UTHASH_VER 2.1.0
 #define VERSION_PATCH beta
-#define VERSION_COMMIT_ID nebukadn
+#define VERSION_COMMIT_ID dkt1-amr
