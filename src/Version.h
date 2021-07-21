e9a1c77026c4ba7b788957414958bafa242abaf1
Pairi Daiza <hpwamr@outlook.com>
2021-07-21 16:16:57 +0200

    + Upd: Version files, Changes.txt and MS VS 2019 version update (v16.10.4)

--- src/Version.h
+++ src/Version.h
@@ -77,7 +77,9 @@
 
 #if defined(_MSC_VER)
     #if (_MSC_VER == 1929)
-        #if (_MSC_FULL_VER >= 192930038)
+        #if (_MSC_FULL_VER >= 192930040)
+            #define VER_CPL     MS Visual C++ 2019 v16.10.4
+        #elif (_MSC_FULL_VER >= 192930038)
             #define VER_CPL     MS Visual C++ 2019 v16.10.3
         #elif (_MSC_FULL_VER >= 192930037)
             #define VER_CPL     MS Visual C++ 2019 v16.10.(0-2)
