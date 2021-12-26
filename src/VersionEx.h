de1443b7bb028632044524c48e831f6a09715d75
Pairi Daiza <hpwamr@outlook.com>
2021-12-26 09:32:05 +0100

    +chg: In Windows 11, running "notepad" from "Run" (Win+R) launches "Notepad3"

--- src/VersionEx.h
+++ src/VersionEx.h
@@ -8,7 +8,7 @@
 #define SAPPNAME "Notepad3"
 #define VERSION_MAJOR 5
 #define VERSION_MINOR 21
-#define VERSION_REV 1222
+#define VERSION_REV 1226
 #define VERSION_BUILD 1
 #define SCINTILLA_VER 515
 #define LEXILLA_VER 514
