e80141f98bb98ac7dad8d8aac4b76b304d7f671e
Pairi Daiza <hpwamr@outlook.com>
2022-09-13 21:42:01 +0200

    +upd: version info for MS VS2022 (v17.3.3) update

--- src/Version.h
+++ src/Version.h
@@ -77,7 +77,9 @@
 
 #if defined(_MSC_VER)
     #if (_MSC_VER == 1933)
-        #if (_MSC_FULL_VER >= 193331629)
+        #if (_MSC_FULL_VER >= 193331630)
+            #define VER_CPL     MS Visual C++ 2022 v17.3.4
+        #elif (_MSC_FULL_VER >= 193331629)
             #define VER_CPL     MS Visual C++ 2022 v17.3.(0-3)
         #endif
     #elif (_MSC_VER == 1932)
