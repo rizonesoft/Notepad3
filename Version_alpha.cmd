342a60225bc5f60c92b389f824e76fdc4b2b5019
METANEOCORTEX\Kotti <rainer.kottenhoff@gmail.com>
2022-11-25 20:33:43 +0100

    master contains current beta development, freeze branch for RC and Release

--- Version_alpha.cmd
+++
@@ -1,7 +0,0 @@
-:: Batch file for ALPHA and XPERIMENTAL version
-@echo off
-setlocal
-set _VERPATCH_=alpha
-echo."_%_VERPATCH_%">.\np3portableapp\_buildname.txt
-Version -VerPatch "%_VERPATCH_%"
-endlocal
