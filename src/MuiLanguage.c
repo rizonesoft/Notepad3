35e44331d6e963769630ee063b7693afe6ebfea5
a234106261 <a234106261@163.com>
2023-01-13 14:07:44 +0800

    Update MuiLanguage.c

--- src/MuiLanguage.c
+++ src/MuiLanguage.c
@@ -81,7 +81,7 @@ grepWinLng_t grepWinLangResName[] = {
     { L"tr-TR",  L".\\lng\\gwLng\\Türkçe (Türkiye) [tr-TR].lang" },
     { L"vi-VN",  L".\\lng\\gwLng\\Tiếng Việt (Việt Nam) [vi-VN].lang" },
     { L"zh-CN",  L".\\lng\\gwLng\\简体中文 (中国大陆) [zh-CN].lang" },
-    { L"zh-TW",  L".\\lng\\gwLng\\正體中文 (中國台灣) [zh-TW].lang" }
+    { L"zh-TW",  L".\\lng\\gwLng\\繁體中文 (中國台灣) [zh-TW].lang" }
 };
 
 unsigned grepWinLang_CountOf() {
@@ -234,7 +234,7 @@ MUILANGUAGE MUI_LanguageDLLs[] = {
     { IDS_MUI_LANG_TR_TR,  L"tr-TR",   L"Türkçe (Türkiye)\t\t\t[%s]",              false, false },
     { IDS_MUI_LANG_VI_VN,  L"vi-VN",   L"Tiếng Việt (Việt Nam)\t\t\t[%s]",         false, false },
     { IDS_MUI_LANG_ZH_CN,  L"zh-CN",   L"简体中文 (中国大陆)\t\t\t[%s]",                false, false },
-    { IDS_MUI_LANG_ZH_TW,  L"zh-TW",   L"正體中文 (中國台灣)\t\t\t[%s]",                false, false }
+    { IDS_MUI_LANG_ZH_TW,  L"zh-TW",   L"繁體中文 (中國台灣)\t\t\t[%s]",                false, false }
 };
 
 //NUM_OF_MUI_LANGUAGES
