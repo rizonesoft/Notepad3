8b3081162449d73a2da2f01e922fad94f88f3acc
Pairi Daiza <hpwamr@outlook.com>
2023-03-24 15:44:16 +0100

    MUI - Check version of MS Visual Studio

--- Notepad3_sln_ASAN.cmd
+++ Notepad3_sln_ASAN.cmd
@@ -2,8 +2,16 @@
 setlocal enableextensions
 set SDIR=%~dp0
 
-::set DEVENV=C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\IDE\devenv.exe
-set DEVENV=C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe
+:: --- Check version of MS Visual Studio ---
+if exist "C:\Program Files\Microsoft Visual Studio\2022\Professional\" (
+    set DEVENV=C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\IDE\devenv.exe
+) else (
+    if exist "C:\Program Files\Microsoft Visual Studio\2022\Community\" (
+        set DEVENV=C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe
+    ) else (
+      goto :END
+    )
+)
 
 set SLN=%SDIR%Notepad3.sln
 
@@ -11,6 +19,7 @@ set ASAN_OPTIONS=windows_hook_legacy_allocators=false
 
 start "devenv" /MAX /B  "%DEVENV%" "%SLN%"
 
+:END
 endlocal
 ::pause
 exit
