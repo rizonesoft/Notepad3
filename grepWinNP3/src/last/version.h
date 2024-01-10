83f42c5c1a0745b19fa9e658e8f4f85d11eb0031
METANEOCORTEX\Kotti <rainer.kottenhoff@gmail.com>
2024-01-10 18:28:49 +0100

    version change for last fix

--- grepWinNP3/src/last/version.h
+++ grepWinNP3/src/last/version.h
@@ -6,13 +6,13 @@
 
 //#pragma message(__LOC__"Run the NAnt script to get proper version info")
 
-#define FILEVER         2, 1, 15, 47
-#define PRODUCTVER      2, 1, 15, 47
-#define STRFILEVER      "2.1.15.47\0"
-#define STRPRODUCTVER   "2.1.15.47\0"
+#define FILEVER         2, 1, 15, 48
+#define PRODUCTVER      2, 1, 15, 48
+#define STRFILEVER      "2.1.15.48\0"
+#define STRPRODUCTVER   "2.1.15.48\0"
 
 #define GREPWIN_VERMAJOR     2
 #define GREPWIN_VERMINOR     1
 #define GREPWIN_VERMICRO     15
-#define GREPWIN_VERBUILD     47
-#define GREPWIN_VERDATE      "2023-06-19"
+#define GREPWIN_VERBUILD     48
+#define GREPWIN_VERDATE      "2024-01-10"
