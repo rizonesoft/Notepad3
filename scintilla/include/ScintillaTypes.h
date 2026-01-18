3a26c0fd672cfc92cfa0f2d7ff6985a5883cc334
Derick Payne <rizonetech@gmail.com>
2026-01-18 02:44:54 +0200

    feat(scintilla): extend FontWeight enum with all DirectWrite values
    Adds full range of font weights to support all DirectWrite variants:
    - Thin (100), ExtraLight (200), Light (300), SemiLight (350)
    - Normal (400), Medium (500)
    - SemiBold (600), Bold (700), ExtraBold (800), Black (900), ExtraBlack (950)

--- scintilla/include/ScintillaTypes.h
+++ scintilla/include/ScintillaTypes.h
@@ -159,11 +159,22 @@ enum class CaseVisible {
 	Camel = 3,
 };
 
+// >>>>>>>>>>>>>>>   BEG NON STD SCI PATCH   >>>>>>>>>>>>>>>
+// Extended FontWeight enum to support all DirectWrite font weight values
 enum class FontWeight {
+	Thin = 100,
+	ExtraLight = 200,
+	Light = 300,
+	SemiLight = 350,
 	Normal = 400,
+	Medium = 500,
 	SemiBold = 600,
 	Bold = 700,
+	ExtraBold = 800,
+	Black = 900,
+	ExtraBlack = 950,
 };
+// <<<<<<<<<<<<<<<   END NON STD SCI PATCH   <<<<<<<<<<<<<<<
 
 enum class FontStretch {
 	UltraCondensed = 1,
