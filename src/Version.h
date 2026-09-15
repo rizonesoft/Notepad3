39c5029273096520e8f796b654521c9c9cb57793
Pairi Daiza <hpwamr@outlook.com>
2026-09-15 23:05:48 +0200

    d: version info for MS VS2026 (v18.10.1) update

--- src/Version.h
+++ src/Version.h
@@ -175,7 +175,7 @@ inline LPCWSTR _Win10BuildToReleaseId(const DWORD build)
 #if defined(_MSC_VER)
     #if (_MSC_VER == 1951)
         #if (_MSC_FULL_VER >= 195136257)
-            #define VER_CPL     MS Visual C++ 2026 v18.10.0
+            #define VER_CPL     MS Visual C++ 2026 v18.10.(0-1)
         #elif (_MSC_FULL_VER >= 195136256)
             #define VER_CPL     MS Visual C++ 2026 v18.9.(0-3)
         #elif (_MSC_FULL_VER >= 195136252)
